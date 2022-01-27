4908d06e7ac65bf396ba43be304a514346c5fb49
colmhaugh <colm.haugh@gmail.com>
2022-01-27 17:03:28 +0000

    Add new pages

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ from taskmanager import app
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT")), # converted to int
+        port=int(os.environ.get("PORT")),
         debug=os.environ.get("DEBUG")
     )
