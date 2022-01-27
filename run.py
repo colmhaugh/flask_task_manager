205629ca5d4b7d0c668847c9a054f79957bd9d66
colmhaugh <colm.haugh@gmail.com>
2022-01-27 13:52:41 +0000

    Initial Commit

---
+++ run.py
@@ -0,0 +1,10 @@
+import os
+from taskmanager import app
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")), # converted to int
+        debug=os.environ.get("DEBUG")
+    )
